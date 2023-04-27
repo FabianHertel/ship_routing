c03b7116fd69fe530930c436c542e6a17b1c62ff
Fabian Hertel <st151599@stud.uni-stuttgart.de>
2023-04-27 18:32:16 +0200

    workaround for par_map_reduce method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use osmpbf::{Element, IndexedReader};
+use osmpbf::{Element, ElementReader, IndexedReader};
 use std::error::Error;
+use std::ffi::OsString;
 use std::time::{SystemTime};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -7,14 +8,67 @@ fn main() -> Result<(), Box<dyn Error>> {
     let arg = std::env::args_os()
         .nth(1)
         .ok_or("need a *.osm.pbf file as argument")?;
-    let mut reader = IndexedReader::from_path(&arg)?;
+
 
     let now = SystemTime::now();
     println!("Counting...");
+
+    let (nodes, ways) = read_pbf_index_reader(&arg);
+    println!("ways:  {}\nnodes: {}", ways, nodes);
+
+    let coastline = read_pbf_element_reader(&arg);
+    println!("coastline:  {}", coastline.len());
+
+    match now.elapsed() {
+        Ok(elapsed) => {
+            println!("{}", elapsed.as_secs());
+        }
+        Err(e) => {
+            println!("Error: {e:?}");
+        }
+    }
+
+    Ok(())
+}
+
+fn read_pbf_element_reader(path: &OsString) -> Vec<(f64, f64)> {
+    let reader = ElementReader::from_path(path);
+
+    match reader.unwrap().par_map_reduce(
+        |element| {
+            match element {
+                Element::Node(node) => [(node.lat(), node.lon())].to_vec(),
+                Element::DenseNode(node) => {
+                    if node.tags().any(|key_value| key_value == ("natural", "coastline")) {
+                        [(node.lat(), node.lon())].to_vec()
+                    } else {
+                        vec![]
+                    }
+                },
+                _ => vec![],
+            }
+        },
+        || [].to_vec(),      // Zero is the identity value for addition
+        |mut a, mut b| {
+            a.append(&mut b);
+            a
+        }   // Sum the partial results
+    ) {
+        Ok(ways) => return ways,
+        Err(e) => {
+            println!("{}", e.to_string());
+            return vec![]
+        }
+    };
+}
+
+fn read_pbf_index_reader(path: &OsString) -> (i32, i32) {
     let mut ways = 0;
     let mut nodes = 0;
 
-    reader.read_ways_and_deps(
+    let mut reader = IndexedReader::from_path(path).unwrap();
+
+    match reader.read_ways_and_deps(
         |way| {
             // Filter ways. Return true if tags contain "building": "yes".
             way.tags().any(|key_value| key_value == ("natural", "coastline"))
@@ -28,18 +82,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                 Element::Relation(_) => {} // should not occur
             }
         },
-    )?;
-
-    println!("ways:  {}\nnodes: {}", ways, nodes);
-
-    match now.elapsed() {
-        Ok(elapsed) => {
-            println!("{}", elapsed.as_secs());
-        }
+    ) {
+        Ok(()) => return (nodes, ways),
         Err(e) => {
-            println!("Error: {e:?}");
+            println!("{}", e.to_string());
+            return (0,0)
         }
-    }
-
-    Ok(())
+    };
 }
