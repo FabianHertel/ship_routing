16218b771fe166ddc004a40ed75bec3c020fceeb
Fabian Hertel <st151599@stud.uni-stuttgart.de>
2023-05-03 15:36:31 +0200

    own algorithm to read pbf + filter nodes

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::error::Error;
 use std::time::{SystemTime};
 
 mod read_pbf;
-use read_pbf::read_pbf_element_reader;
-use read_pbf::read_pbf_index_reader;
+
+use crate::read_pbf::waypoints_coastline_parallel;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Read command line argument and create IndexedReader
@@ -13,21 +13,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 
     let now = SystemTime::now();
-    println!("Counting...");
+    println!("Importing...");
 
-    let (nodes, ways) = read_pbf_index_reader(&arg);
-    println!("ways:  {}\nnodes: {}", ways, nodes);
-
-    let coastline = read_pbf_element_reader(&arg);
+    let coastline = waypoints_coastline_parallel(&arg);
     println!("coastline:  {}", coastline.len());
 
     match now.elapsed() {
-        Ok(elapsed) => {
-            println!("{}", elapsed.as_secs());
-        }
-        Err(e) => {
-            println!("Error: {e:?}");
-        }
+        Ok(elapsed) => {println!("Import and filter time: {}", elapsed.as_secs());}
+        Err(e) => {println!("Error: {e:?}");}
     }
 
     Ok(())
--- src/read_pbf.rs
+++ src/read_pbf.rs
@@ -1,19 +1,21 @@
 use osmpbf::{Element, ElementReader, IndexedReader};
-use std::ffi::OsString;
+use std::{ffi::OsString, collections::{HashSet}};
 
-pub fn read_pbf_element_reader(path: &OsString) -> Vec<(f64, f64)> {
+pub fn waypoints_coastline_parallel(path: &OsString) -> Vec<(f64, f64)> {
     let reader = ElementReader::from_path(path);
+    let waypoint_refs = par_coastline_as_refs(path);
+    let node_set: HashSet<i64> = waypoint_refs.into_iter().collect();
 
     match reader.unwrap().par_map_reduce(
         |element| {
             match element {
-                Element::Node(node) => [(node.lat(), node.lon())].to_vec(),
                 Element::DenseNode(node) => {
-                    if node.tags().any(|key_value| key_value == ("natural", "coastline")) {
-                        [(node.lat(), node.lon())].to_vec()
+                    if node_set.contains(&node.id) {
+                        vec![(node.lat(), node.lon())]
                     } else {
                         vec![]
                     }
+                    
                 },
                 _ => vec![],
             }
@@ -32,7 +34,38 @@ pub fn read_pbf_element_reader(path: &OsString) -> Vec<(f64, f64)> {
     };
 }
 
-pub fn read_pbf_index_reader(path: &OsString) -> (i32, i32) {
+fn par_coastline_as_refs(path: &OsString) -> Vec<i64> {
+    let reader = ElementReader::from_path(path);
+
+    match reader.unwrap().par_map_reduce(
+        |element| {
+            match element {
+                Element::Way(way) => {
+                    if way.tags().any(|key_value| key_value == ("natural", "coastline")) {
+                        way.refs().collect()
+                    } else {
+                        vec![]
+                    }
+                },
+                _ => vec![],
+            }
+        },
+        || [].to_vec(),      // Zero is the identity value for addition
+        |mut a, mut b| {
+            a.append(&mut b);
+            a
+        }   // Sum the partial results
+    ) {
+        Ok(ways) => return ways.to_vec(),
+        Err(e) => {
+            println!("{}", e.to_string());
+            return vec![]
+        }
+    };
+}
+
+#[allow(dead_code)]
+pub fn waypoints_coastline_lib(path: &OsString) -> (i32, i32) {
     let mut ways = 0;
     let mut nodes = 0;
 
@@ -41,12 +74,15 @@ pub fn read_pbf_index_reader(path: &OsString) -> (i32, i32) {
     match reader.read_ways_and_deps(
         |way| {
             // Filter ways. Return true if tags contain "building": "yes".
-            way.tags().any(|key_value| key_value == ("natural", "coastline"))
+            way.tags().any(|key_value| key_value == ("natural", "coastline"));
+            true
         },
         |element| {
             // Increment counter for ways and nodes
             match element {
-                Element::Way(_way) => ways += 1,
+                Element::Way(_way) => {
+                    ways += 1
+                },
                 Element::Node(_node) => nodes += 1,
                 Element::DenseNode(_dense_node) => nodes += 1,
                 Element::Relation(_) => {} // should not occur
