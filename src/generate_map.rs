fbe6b9fd3615a6c8e1bf4c4a4c995981892de868
Fabian Hertel <st151599@stud.uni-stuttgart.de>
2023-05-31 14:17:05 +0200

    fix ditance calc bug

--- src/generate_map.rs
+++ src/generate_map.rs
@@ -12,7 +12,6 @@ pub fn generate_map() -> Result<(), Box<dyn Error>> {
     println!("2/?: Precalculations for islands/continents ...");
     let now = SystemTime::now();
     let islands: Vec<Island> = coastlines.iter().map(|e| Island::new(e.to_owned())).collect();
-    println!("2/?: Biggest Continent: center={},{}; distance={}", islands[10].cog[0], islands[10].cog[1], islands[10].max_dist_from_cog);
     println!("2/?: Finished precalculations in {} sek", now.elapsed()?.as_secs());
 
     let now = SystemTime::now();
@@ -72,7 +71,7 @@ fn distance_between(x: &Vec<f64>, y:&Vec<f64>) -> f64 {
     let φ1 = x[1] * PI/180.0; // φ, λ in radians
     let φ2 = y[1] * PI/180.0;
     let dφ = (y[1]-x[1]) * PI/180.0;
-    let dλ = (y[0]-y[1]) * PI/180.0;
+    let dλ = (y[0]-x[0]) * PI/180.0;
     const EARTH_RADIUS: f64 = 6371.0;
 
     let haversine = (dφ/2.0).sin().powi(2) + φ1.cos() * φ2.cos() * (dλ/2.0).sin().powi(2);
@@ -109,18 +108,17 @@ pub fn read_geojsons(prefix: &str) -> Vec<Vec<Vec<f64>>> {
  * Note: South pole seems to marked as water in OSM. Antartica seems to end there.
  */
 fn point_in_polygon_test(lon: f64, lat: f64, polygons: &Vec<Island>) -> bool {
-    let mut n_of_checks = 0;
 
     for island in polygons {
         if island.in_range(&vec![lon, lat]) {
             let polygon = &island.coastline;
             let mut in_water = false;
-            n_of_checks += 1;
+            // println!("Island center: {}, {}; max_dist_from_cog: {}; point distance: {}", island.cog[0], island.cog[1], island.max_dist_from_cog, distance_between(&island.cog, &vec![lon, lat]));
             for j in 1..polygon.len() {       // ignore first point in polygon, because first and last will be the same
                 let i = j - 1;
                 if (polygon[i][0] > lon) != (polygon[j][0] > lon) {   // check if given point has lon between start and end point of edge
                     if (polygon[i][1] < lat) && (polygon[j][1] < lat) {     // if both start and end point are south, the going south will cross
-                        println!("Line crossed: {}, {}; {}, {}", polygon[i][0], polygon[i][1], polygon[j][0], polygon[j][1]);
+                        // println!("Line crossed: {}, {}; {}, {}", polygon[i][0], polygon[i][1], polygon[j][0], polygon[j][1]);
                         in_water = !in_water;
                     } else if (polygon[i][1] < lat) || (polygon[j][1] < lat) {      // if one of start and end point are south, we have to check... (happens rarely for coastline)
                         let slope = (lat-polygon[i][1])*(polygon[j][0]-polygon[i][0])-(polygon[j][1]-polygon[i][1])*(lon-polygon[i][0]);
@@ -132,11 +130,9 @@ fn point_in_polygon_test(lon: f64, lat: f64, polygons: &Vec<Island>) -> bool {
                 }
             }
             if in_water {
-                println!("Found one after {} checks", n_of_checks);
                 return true
             }
         }
     }
-    println!("Didn't find after {} checks", n_of_checks);
     return false;
 }
