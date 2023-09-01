94ba2c34724f43c184cd731b891d85193ae3089d
Fabian Hertel <st151599@stud.uni-stuttgart.de>
2023-09-01 12:35:25 +0200

    multiple contractions with error

--- ship-routing/src/ch.rs
+++ ship-routing/src/ch.rs
@@ -7,73 +7,23 @@ pub fn ch_precalculations(graph: &Graph) {
     let mut contracting_graph = CHGraph::from_graph(graph);
     println!("Graph of size {} and {} edges", contracting_graph.n_nodes(), contracting_graph.n_edges());
     let mut l_counter: Vec<usize> = vec![0; contracting_graph.n_nodes()];
-    let mut hopcount_sums: Vec<usize> = graph.nodes.iter().map(|node| graph.get_outgoing_edges(node.id).len()).collect();
-    let mut hopcount_edges: Vec<usize> = vec![1; graph.n_edges()];
-    let mut priority_queue = BinaryMinHeap::with_capacity(contracting_graph.n_nodes());
-
+    let mut priority_queue: BinaryMinHeap = BinaryMinHeap::with_capacity(contracting_graph.n_nodes());
     let mut importance: Vec<f32> = vec![0.0; contracting_graph.n_nodes()];
 
-    // initialize heuristic
-    for (_node_id, node) in &contracting_graph.nodes {       // TODO: parallel
-        let mut hopcount_sum = 0;
-        let mut insert_edges: Vec<(usize, usize, CHEdge)> = vec![];
-        {
-            let neighbours = &node.borrow().neighbours;
-            let neighbour_ids: Vec<&usize> = neighbours.keys().collect();
+    let mut update_nodes = contracting_graph.nodes.keys().map(|node| *node).collect();
 
-            for i in 0..neighbour_ids.len() {
-                for j in (i+1)..neighbour_ids.len() {
-                    let i_edge = neighbours.get(neighbour_ids[i]).unwrap();
-                    let j_edge = neighbours.get(neighbour_ids[j]).unwrap();
-                    let edge_sum =  i_edge.dist + j_edge.dist;
-                    let src_node = neighbour_ids[i];
-                    let tgt_node = neighbour_ids[j];
-                    let witness_search = run_witness_search(
-                        *src_node, *tgt_node, &contracting_graph, Some(50usize), edge_sum, node.borrow().id);
-                    if witness_search > edge_sum {
-                        let hopcount = i_edge.hopcount + j_edge.hopcount;
-                        insert_edges.push((*neighbour_ids[i], *neighbour_ids[j], CHEdge {dist: edge_sum, hopcount}));
-                        hopcount_sum += hopcount;
-                    }
-                }
-            }
-            importance[node.borrow().id] = l_counter[node.borrow().id] as f32 + insert_edges.len() as f32 / neighbour_ids.len() as f32 + hopcount_sum as f32 / hopcount_sums[node.borrow().id] as f32;
-            priority_queue.push(node.borrow().id, &importance);
-        } {
-            node.borrow_mut().insertions = Insertions { hopcount_sum, insert_edges };
-        }
+    while contracting_graph.n_nodes() as f32 > graph.n_nodes() as f32 * 0.1 {
+        contracting_graph.update_importance_of(&mut importance, &update_nodes, &mut priority_queue, &l_counter);
+
+        let (independent_set, affected_nodes) = contracting_graph.find_best_independent_set(&mut priority_queue, &importance);
+        contracting_graph.nodes_to_clipboard(graph, &independent_set);
+        update_nodes = affected_nodes;
+
+        contracting_graph.contract_nodes(independent_set, &mut l_counter);
+        println!("Graph of size {} and {} edges", contracting_graph.n_nodes(), contracting_graph.n_edges());
+        contracting_graph.edges_to_clipboard(graph);
     }
-    // println!("{:?}", importance);
-    println!("Initial importance calculated, start contraction");
-
-    // find independent subset
-    let mut contracted_nodes: Vec<usize> = vec![];
-    let mut neighbour_nodes: HashSet<usize> = HashSet::new();
-    while !priority_queue.is_empty() {
-        let next_node = priority_queue.pop(&importance);
-        if !neighbour_nodes.contains(&next_node) {
-            contracted_nodes.push(next_node);
-            neighbour_nodes.extend(contracting_graph.borrow_node(next_node).neighbours.iter().map(|(tgt, _)| tgt));
-        } else {
-            // optional: without break not optimal, but faster
-            break;
-        }
-    }
-    let node_string = format!("[{}]", contracted_nodes.iter().map(|node_id| format!("[{},{}]", graph.get_node(*node_id).lon, graph.get_node(*node_id).lat)).reduce(|e,f| e+","+&f).unwrap());
-    cli_clipboard::set_contents(node_string.to_owned()).unwrap();
-
-    let first_contraction = contracted_nodes[0];
-    {
-            let first_contraction_node = contracting_graph.borrow_node(first_contraction);
-            println!("First out of {} nodes, node {}, with {} neigbours and {} inserted edges, removed hopcounts: {} and inserted hopcounts: {}",
-            contracted_nodes.len(), first_contraction, first_contraction_node.neighbours.len(),
-            first_contraction_node.insertions.insert_edges.len(), hopcount_sums[first_contraction], first_contraction_node.insertions.hopcount_sum
-        );
-    }
-
-    contracting_graph.contract_nodes(contracted_nodes);
-    println!("Graph of size {} and {} edges", contracting_graph.n_nodes(), contracting_graph.n_edges());
-    contracting_graph.edges_to_clipboard(graph);
+    
 }
 
 /// Run a Dijkstra from the source coodinates to the target coordinates
@@ -107,7 +57,7 @@ impl CHGraph {
                 id: node.id,
                 // l: 0,
                 // houcount_sum: graph.get_outgoing_edges(node.id).len(),
-                insertions: Insertions { hopcount_sum: usize::MAX, insert_edges: vec![] }
+                insertions: vec![]
             }));
         }
         for edge in &graph.edges {
@@ -129,19 +79,79 @@ impl CHGraph {
         return self.nodes.get(&id).expect(&format!("No node with id {}", id)).borrow_mut();
     }
 
-    pub fn contract_nodes(&mut self, nodes: Vec<usize>) {
+    pub fn update_importance_of(&self, importance: &mut Vec<f32>, update_nodes: &HashSet<usize>, priority_queue: &mut BinaryMinHeap, l_counter: &Vec<usize>) {
+        // initialize heuristic
+        for node_id in update_nodes {       // TODO: parallel
+            let mut hopcount_sum_insert = 0;
+            let mut hopcount_sum = 0f32;
+            let mut insert_edges: Vec<(usize, usize, CHEdge)> = vec![];
+            {
+                let node = self.borrow_node(*node_id);
+                let neighbours = &node.neighbours;
+                let neighbour_ids: Vec<&usize> = neighbours.keys().collect();
+
+                hopcount_sum += neighbours.iter().fold(0.0, |base, e| base + e.1.hopcount as f32);
+
+                for i in 0..neighbour_ids.len() {
+                    for j in (i+1)..neighbour_ids.len() {
+                        let i_edge = neighbours.get(neighbour_ids[i]).unwrap();
+                        let j_edge = neighbours.get(neighbour_ids[j]).unwrap();
+                        let edge_sum =  i_edge.dist + j_edge.dist;
+                        let src_node = neighbour_ids[i];
+                        let tgt_node = neighbour_ids[j];
+                        let witness_search = run_witness_search(
+                            *src_node, *tgt_node, self, Some(50usize), edge_sum, *node_id);
+                        if witness_search > edge_sum {
+                            let hopcount = i_edge.hopcount + j_edge.hopcount;
+                            insert_edges.push((*neighbour_ids[i], *neighbour_ids[j], CHEdge {dist: edge_sum, hopcount}));
+                            hopcount_sum_insert += hopcount;
+                        }
+                    }
+                }
+                importance[node.id] = l_counter[node.id] as f32 + insert_edges.len() as f32 / neighbour_ids.len() as f32 + hopcount_sum_insert as f32 / hopcount_sum as f32;
+                priority_queue.push(node.id, &importance);
+            } {
+                self.borrow_mut_node(*node_id).insertions = insert_edges;
+            }
+        }
+        // println!("{:?}", importance);
+        println!("Initial importance calculated, start contraction");
+    }
+
+    pub fn find_best_independent_set(&self, priority_queue: &mut BinaryMinHeap, importance: &Vec<f32>) -> (Vec<usize>, HashSet<usize>) {
+        let mut independent_set: Vec<usize> = vec![];
+        let mut neighbour_nodes: HashSet<usize> = HashSet::new();
+        let mut importance_limit = f32::MAX;
+        while !priority_queue.is_empty() {
+            let next_node = priority_queue.pop(&importance);
+            if importance[next_node] > importance_limit + 1.0 {
+                // optional: without break not optimal, but faster
+                break;
+            }
+            if !neighbour_nodes.contains(&next_node) {
+                independent_set.push(next_node);
+                neighbour_nodes.extend(self.borrow_node(next_node).neighbours.iter().map(|(tgt, _)| tgt));
+            } else {
+                importance_limit = importance[next_node];
+            }
+        }
+        return (independent_set, neighbour_nodes);
+    }
+
+    pub fn contract_nodes(&mut self, nodes: Vec<usize>, l_counter: &mut Vec<usize>) {
         nodes.iter().for_each(|node_id| {
             // println!("remove node {}", node_id);
             let removed_node = self.nodes.remove(node_id).expect("contraction node doesn't exist");
 
-            // remove from neighbours
+            // remove from neighbours and increment their l counter
             for (tgt, _) in &removed_node.borrow().neighbours {
                 // println!("remove {} from {}", node_id, tgt);
                 self.borrow_mut_node(*tgt).neighbours.remove(&node_id);
+                l_counter[*tgt] += 1;
             }
 
             // add insertions
-            for (src, tgt, new_edge) in &removed_node.borrow().insertions.insert_edges {
+            for (src, tgt, new_edge) in &removed_node.borrow().insertions {
                 // println!("insert edge from {} to {}", src, tgt);
                 self.borrow_mut_node(*tgt).neighbours.insert(*src, new_edge.clone());
                 self.borrow_mut_node(*src).neighbours.insert(*tgt, new_edge.to_owned());
@@ -162,6 +172,11 @@ impl CHGraph {
         cli_clipboard::set_contents(the_string.to_owned()).unwrap();
         assert_eq!(cli_clipboard::get_contents().unwrap(), the_string);
     }
+
+    pub fn nodes_to_clipboard(&self, graph: &Graph, nodes: &Vec<usize>) {
+        let node_string = format!("[{}]", nodes.iter().map(|node_id| format!("[{},{}]", graph.get_node(*node_id).lon, graph.get_node(*node_id).lat)).reduce(|e,f| e+","+&f).unwrap());
+        cli_clipboard::set_contents(node_string.to_owned()).unwrap();
+    }
 }
 
 pub struct CHNode {
@@ -172,10 +187,7 @@ pub struct CHNode {
     insertions: Insertions
 }
 
-pub struct Insertions {
-    hopcount_sum: usize,
-    insert_edges: Vec<(usize, usize, CHEdge)>
-}
+type Insertions = Vec<(usize, usize, CHEdge)>;
 
 #[derive(Clone)]
 pub struct CHEdge {pub hopcount: usize, pub dist: u32}
