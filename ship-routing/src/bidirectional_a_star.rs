2baad3e3bfbf166d21719334acfc5cf789db2e62
Fabian Hertel <st151599@stud.uni-stuttgart.de>
2023-09-17 14:14:58 +0200

    ch a* with map instead vec

--- ship-routing/src/bidirectional_a_star.rs
+++ ship-routing/src/bidirectional_a_star.rs
@@ -1,5 +1,5 @@
-use std::time::SystemTime;
-use crate::binary_minheap::BinaryMinHeap;
+use std::{time::SystemTime, collections::HashMap};
+use crate::binary_minheap_map::BinaryMinHeapMap;
 use graph_lib::{ShortestPathResult, Graph, Node};
 
 /**
@@ -10,17 +10,13 @@ pub fn run_bidirectional_a_star(src_node: &Node, tgt_node: &Node, graph: &Graph)
     let start_end_dist = src_node.distance_to_node(src_node).floor() as u32;
     
     // init forward
-    let mut heuristic_forward = HeuristicalDistances::init(
-        graph.n_nodes(), src_node.id, start_end_dist
-    );
-    let mut pq_forward = BinaryMinHeap::with_capacity(graph.n_nodes());
+    let mut heuristic_forward = HeuristicalDistances::init(src_node.id, start_end_dist);
+    let mut pq_forward = BinaryMinHeapMap::with_capacity(graph.n_nodes());
     pq_forward.push(src_node.id, &heuristic_forward.g_plus_h);
 
     // init backward
-    let mut heuristic_backward = HeuristicalDistances::init(
-        graph.n_nodes(), tgt_node.id, start_end_dist
-    );
-    let mut pq_backward = BinaryMinHeap::with_capacity(graph.n_nodes());
+    let mut heuristic_backward = HeuristicalDistances::init(tgt_node.id, start_end_dist);
+    let mut pq_backward = BinaryMinHeapMap::with_capacity(graph.n_nodes());
     pq_backward.push(tgt_node.id, &heuristic_backward.g_plus_h);
 
     let mut visited_nodes = 0;
@@ -36,9 +32,9 @@ pub fn run_bidirectional_a_star(src_node: &Node, tgt_node: &Node, graph: &Graph)
         // try forward
         if !forward_finished && !pq_forward.is_empty() {
             let node_id_forward = pq_forward.pop(&heuristic_forward.g_plus_h);
-            if heuristic_forward.g_plus_h[node_id_forward] < best_result_so_far {
-                if heuristic_forward.g[node_id_forward] != u32::MAX && heuristic_backward.g[node_id_forward] != u32::MAX {
-                    let possible_result = heuristic_forward.g[node_id_forward] + heuristic_backward.g[node_id_forward];
+            if heuristic_forward.g_plus_h[&node_id_forward] < best_result_so_far {
+                if heuristic_backward.g.contains_key(&node_id_forward) {
+                    let possible_result = heuristic_forward.g[&node_id_forward] + heuristic_backward.g[&node_id_forward];
                     if possible_result < best_result_so_far {
                         best_result_so_far = possible_result;
                         node_id_middle = Some(node_id_forward);
@@ -55,9 +51,9 @@ pub fn run_bidirectional_a_star(src_node: &Node, tgt_node: &Node, graph: &Graph)
         // try backward
         if !backward_finished && !pq_backward.is_empty()  {
             let node_id_backward = pq_backward.pop(&heuristic_backward.g_plus_h);
-            if heuristic_backward.g_plus_h[node_id_backward] < best_result_so_far {
-                if heuristic_forward.g[node_id_backward] != u32::MAX && heuristic_backward.g[node_id_backward] != u32::MAX {
-                    let possible_result = heuristic_forward.g[node_id_backward] + heuristic_backward.g[node_id_backward];
+            if heuristic_backward.g_plus_h[&node_id_backward] < best_result_so_far {
+                if heuristic_forward.g.contains_key(&node_id_backward) {
+                    let possible_result = heuristic_forward.g[&node_id_backward] + heuristic_backward.g[&node_id_backward];
                     if possible_result < best_result_so_far {
                         best_result_so_far = possible_result;
                         node_id_middle = Some(node_id_backward);
@@ -97,20 +93,20 @@ pub fn run_bidirectional_a_star(src_node: &Node, tgt_node: &Node, graph: &Graph)
 /**
  * visits a node, which means it processes all its edges and updates the function g(a)+h(a) of all neighbours
 */
-fn process_edges(graph: &Graph, visiting_node_id: usize, heuristic_dists: &mut HeuristicalDistances, pq: &mut BinaryMinHeap, tgt_node: &Node) {
-    let visiting_node_dist = heuristic_dists.g_plus_h[visiting_node_id] - heuristic_dists.heuristic[visiting_node_id];
+fn process_edges(graph: &Graph, visiting_node_id: usize, heuristic_dists: &mut HeuristicalDistances, pq: &mut BinaryMinHeapMap, tgt_node: &Node) {
+    let visiting_node_dist = heuristic_dists.g_plus_h[&visiting_node_id] - heuristic_dists.heuristic[&visiting_node_id];
     for edge in graph.get_outgoing_edges(visiting_node_id) {
         // if not calculated already, the heuristic is calculated here
-        if heuristic_dists.heuristic[edge.tgt] == u32::MAX {
-            heuristic_dists.heuristic[edge.tgt] = graph.get_node(edge.tgt).distance_to_node(tgt_node).floor() as u32;
+        if !heuristic_dists.heuristic.contains_key(&edge.tgt) {
+            heuristic_dists.heuristic.insert(edge.tgt,  graph.get_node(edge.tgt).distance_to_node(tgt_node).floor() as u32);
         }
-        let neighbour_node_dist = visiting_node_dist + edge.dist + heuristic_dists.heuristic[edge.tgt];
-
+        let neighbour_node_dist = visiting_node_dist + edge.dist + heuristic_dists.heuristic[&edge.tgt];
+        
         // if way over visiting node is the best so far, the neighbour will be updated
-        if neighbour_node_dist < heuristic_dists.g_plus_h[edge.tgt] {
-            heuristic_dists.g[edge.tgt] = visiting_node_dist + edge.dist;
-            heuristic_dists.g_plus_h[edge.tgt] = neighbour_node_dist;
-            heuristic_dists.preds[edge.tgt] = visiting_node_id;
+        if !heuristic_dists.g_plus_h.contains_key(&edge.tgt) || neighbour_node_dist < heuristic_dists.g_plus_h[&edge.tgt] {
+            heuristic_dists.g.insert(edge.tgt, visiting_node_dist + edge.dist);
+            heuristic_dists.g_plus_h.insert(edge.tgt, neighbour_node_dist);
+            heuristic_dists.preds.insert(edge.tgt, visiting_node_id);
 
             pq.insert_or_update(edge.tgt, &heuristic_dists.g_plus_h);
         }
@@ -121,25 +117,25 @@ fn process_edges(graph: &Graph, visiting_node_id: usize, heuristic_dists: &mut H
 
 #[derive(Debug)]
 pub struct HeuristicalDistances {
-    g_plus_h: Vec<u32>,
-    g: Vec<u32>,
-    preds: Vec<usize>,
-    heuristic: Vec<u32>
+    g_plus_h: HashMap<usize, u32>,
+    g: HashMap<usize, u32>,
+    preds: HashMap<usize, usize>,
+    heuristic: HashMap<usize, u32>
 }
 
 impl HeuristicalDistances {
     /// Creates a new `HeuristicalDistances` instance for given graph size with dist to src 0.0 and else infinity
-    fn init(num_nodes: usize, src_id: usize, start_end_dist: u32) -> Self {
-        let mut g = vec![u32::MAX; num_nodes];
-        let mut g_plus_h = vec![u32::MAX; num_nodes];
-        let mut heuristic = vec![u32::MAX; num_nodes];
-        heuristic[src_id] = start_end_dist;
-        g_plus_h[src_id] = 0 + start_end_dist;
-        g[src_id] = 0;
+    fn init(src_id: usize, start_end_dist: u32) -> Self {
+        let mut g = HashMap::new();
+        let mut g_plus_h = HashMap::new();
+        let mut heuristic = HashMap::new();
+        heuristic.insert(src_id, start_end_dist);
+        g_plus_h.insert(src_id, 0 + start_end_dist);
+        g.insert(src_id, 0);
         Self {
             g_plus_h,
             g,
-            preds: vec![usize::MAX; num_nodes],
+            preds: HashMap::new(),
             heuristic
         }
     }
@@ -148,13 +144,13 @@ impl HeuristicalDistances {
     /// This method assumes that the target can be reached from the source, otherwise it will
     /// output a path that solely consists of the target.
     pub fn build_path(&self, graph: &Graph, tgt_id: usize) -> Option<Vec<Node>> {
-        if self.g_plus_h[tgt_id] == u32::MAX {return None}
+        if !self.preds.contains_key(&tgt_id) {return None}
         let mut path = vec![];
         let mut curr_pred = tgt_id;
         // source node has no predecessor
         while curr_pred < usize::MAX {
             path.push(graph.get_node(curr_pred).clone());
-            curr_pred = self.preds[curr_pred];
+            curr_pred = *self.preds.get(&curr_pred).unwrap();
         }
         path.reverse();
         return Some(path)
